e90fd0fd5ad88ea42ab9e708e64d59f4ed41d986
Dio Lemos <pedro.devsc@outlook.com>
2023-05-10 21:46:41 -0300

    create rand int importing standard c library

--- number.cpp
+++ number.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include <string>
+#include <cstdlib> 
+#include <ctime> 
 using namespace std;
 
 int main()
 {
-    cout<< "Hello world";
+    cout<< "Welcome.";
+    srand(time(nullptr));
+    int randomNumber = rand() % 100 + 1;
+    int tries = 0;
+    char difficulty ;
+
+    bool isGameOve = false;
+
+
+    // Ask user for difficulty
+    cout<< "Chose The difficulty. Type 'h' for Hard or 'e' for Easy.";
+    cin >> difficulty;
+    if(difficulty == 'h'){
+        tries = 10;
+    }else if(difficulty == 'e'){
+        tries = 15;
+    }
+
+    
 }
