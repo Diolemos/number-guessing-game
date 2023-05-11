c462e6ad7f4555839b2f90175053ebd914481f0b
Dio Lemos <pedro.devsc@outlook.com>
2023-05-11 00:37:32 -0300

    add the c++ version of the game

--- number.cpp
+++ number.cpp
@@ -3,16 +3,16 @@
 #include <cstdlib> 
 #include <ctime> 
 using namespace std;
-
-int main()
-{
-    cout<< "Welcome.";
+void play_game(){
+        
+         cout<< "Welcome.";
     srand(time(nullptr));
     int randomNumber = rand() % 100 + 1;
     int tries = 0;
+    int userGuess;
     char difficulty ;
 
-    bool isGameOve = false;
+    bool isGameOver = false;
 
 
     // Ask user for difficulty
@@ -24,5 +24,39 @@ int main()
         tries = 15;
     }
 
+    while (tries > 0 and isGameOver == false){
+        cout<< "Chose a number between 1 and 100 \n";
+        cin>> userGuess ;
+        
+        if(userGuess == randomNumber){
+            cout<<"You got it!! the answer is" <<userGuess;
+            isGameOver = true;
+        }else if(userGuess < randomNumber){
+            tries --;
+           cout << "Too low, you can still try " << tries << " time(s)";
+
+        }else if(userGuess > randomNumber){
+            tries --;
+           cout << "Too High, you can still try " << tries << " time(s)";
+
+        }
+    }
+
+    if(tries == 0){
+        cout<<"You ran out of tries. Better luck next time.";
+    }
+    }
+int main()
+{   char wannaPlay ;
+    cout<<"Would you like to play a guessing game? 'y' or 'n'";
+    cin>>wannaPlay;
+    while(wannaPlay == 'y'){
+        play_game();
+        cout<<"Would you like to play again? 'y' or 'n'?";
+        cin>>wannaPlay;
+
+    }
+    return 0;
     
+   
 }
