624c8562a0a1bbbfd124234cc226eec63c99fe4d
Dio Lemos <pedro.devsc@outlook.com>
2023-07-01 13:38:39 -0300

    created view function and too_high.html The 'guessing game' view function takes 'user_guess' as parameter and it is used in its route decorator function to set the path. Inside this function lies the logic to check if the guess is correct and return a page accordingly. - created too_high.html and image file

--- flask/server.py
+++ flask/server.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+import random
+
+secret_number = random.randint(1,10)
 
 app = Flask(__name__)
 
 @app.route("/")
 def view_home():
     return render_template("index.html")
+
+
+@app.route('/<string:user_guess>')
+def guessing_game(user_guess):
+
+    
+    if int(user_guess) < 1 or int(user_guess) > 10:
+        return "<h1>out of bounds, page not Found </h1>"
+           
+    
+    if int(user_guess) < secret_number:
+        return "<h1>too low, try again </h1>"
+    elif int(user_guess) > secret_number:
+        return render_template("too_high.html")
+    else:
+        return "<h1>You got it! </h1>"
 
 
 if __name__ == "__main__":
